0420589de57138651e0aaaa1f2483dc956922b8a
Putt <57494734+PuttTim@users.noreply.github.com>
2021-07-23 23:08:45 +0800

    Updated PlayerFragment
    More explaination for VMs
    
    Took 14 minutes

--- app/src/main/java/com/example/mastif/Fragments/PlayerFragment.java
+++ app/src/main/java/com/example/mastif/Fragments/PlayerFragment.java
@@ -1,8 +1,11 @@
 package com.example.mastif.Fragments;
 
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
@@ -20,36 +23,44 @@ import java.util.List;
 
 
 public class PlayerFragment extends Fragment {
-
     private FragmentPlayerBinding B;
-    private View.OnClickListener onClickListener;
+    private PlayerViewModel mPlayerVM;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         B = FragmentPlayerBinding.inflate(inflater, container, false);
 
-        PlayerViewModel playerVM = new ViewModelProvider(requireActivity()).get(PlayerViewModel.class);
-        SharedViewModel sharedVM = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        mPlayerVM = new ViewModelProvider(requireActivity()).get(PlayerViewModel.class);
+        mPlayerVM.getPlayer().observe(getViewLifecycleOwner(), new Observer<MediaPlayer>() {
+            @Override
+            public void onChanged(MediaPlayer mediaPlayer) {
 
-        playerVM.getPlayer().observe(getViewLifecycleOwner(), this::logD);
+            }
+        });
+//        mPlayerVM.pauseSong();
 
-        playerVM.pauseSong();
+        Runnable x = () -> System.out.println("HI");
 
+        System.out.println("HI");
 
-        B.btnPause.setOnClickListener(playerVM.pauseSong());
+        B.btnPause.setOnClickListener((v) -> {
+
+        });
+
+        DialogInterface.OnClickListener c = (dialog, which) -> {
+
+        };
 
         // Inflate the layout for this fragment
         return B.getRoot();
     }
 
-    private void logD(List<Song> list) {
+    private void logD(MediaPlayer list) {
         Log.d("anything", list.toString());
     }
 
     public void onPlayClick () {
-        B.btnPause.setOnClickListener(playerVM.new);
-        playerVM
+//        B.btnPause.setOnClickListener(v -> mPlayerVM.pauseSong());
     }
 
 
